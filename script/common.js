bce89e0058c9cc188b144cf0798a7467e7e1e6c8
won <won00yeom@naver.com>
2024-01-18 18:14:17 +0900

    header JS 시작

---
+++ script/common.js
@@ -0,0 +1,9 @@
+const header = document.querySelector('header')
+// const nav = document.querySelector('nav')
+console.log(header)
+const lnbBg = document.querySelectorAll('.lnbBg')
+console.log(lnbBg)
+const subAllBg = document.querySelector('.subAllBg')
+
+subAllBg.style.height = '0' // 390px
+// lnbBg[lbg].style.height = '0'
