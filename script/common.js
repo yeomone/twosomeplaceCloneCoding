a4ee7fd9bf5920248717ad279cdeaeac2a0c92de
won <won00yeom@naver.com>
2024-01-19 17:22:47 +0900

    header JS 애니메이션

--- script/common.js
+++ script/common.js
@@ -1,9 +1,66 @@
 const header = document.querySelector('header')
-// const nav = document.querySelector('nav')
-console.log(header)
 const lnbBg = document.querySelectorAll('.lnbBg')
-console.log(lnbBg)
 const subAllBg = document.querySelector('.subAllBg')
+const gnb = document.querySelectorAll('.gnb > li > a')
+const lang = document.querySelectorAll('.lang')
+const logoDefault = document.querySelector('.default')
+const logoHvr = document.querySelector('.hvr')
+const allMenuDefault = document.querySelector('.allMenuDefault')
+const allMenuHvr = document.querySelector('.allMenuHvr')
+// allMenuDefault
+// allMenuHvr
+// default
+// hvr
 
-subAllBg.style.height = '0' // 390px
-// lnbBg[lbg].style.height = '0'
+for(let l of lnbBg){l.style.display = 'none';}
+subAllBg.style.height = '0'
+subAllBg.style.transition = 'height 0.3s'
+
+logoDefault.style.display = 'block'
+logoHvr.style.display = 'none'
+
+allMenuDefault.style.display = 'block'
+allMenuHvr.style.display = 'none'
+
+/* for(let h of header){
+    for(let l of subAllBg){
+        header.addEventListener('mouseover',()=>{
+            l.style.height = '500px'
+        })
+    }
+} */
+
+header.addEventListener('mouseover',()=>{
+    logoDefault.style.display = 'none'
+    logoHvr.style.display = 'block'
+    allMenuDefault.style.display = 'none'
+    allMenuHvr.style.display = 'block'
+
+    subAllBg.style.opacity = '1'
+    subAllBg.style.height = '390px'
+    subAllBg.style.transition = 'all 0.3s'
+
+    for(let l of lnbBg){l.style.display = 'block';}
+    for(let g of gnb){g.style.color = '#000';}
+    for(let la of lang){la.style.color = '#000';}
+    for(let la of lang){la.style.border = '1px solid #000';}
+    for(let g of gnb){g.style.transition = 'color 0.3s';}
+    for(let la of lang){la.style.transition = 'all 0.3s';}
+})
+header.addEventListener('mouseout',()=>{
+    logoDefault.style.display = 'block'
+    logoHvr.style.display = 'none'
+    allMenuDefault.style.display = 'block'
+    allMenuHvr.style.display = 'none'
+
+    subAllBg.style.opacity = '0'
+    subAllBg.style.height = '100px'
+    subAllBg.style.transition = 'all 0.3s'
+
+    for(let l of lnbBg){l.style.display = 'none';}
+    for(let g of gnb){g.style.color = '#fff';}
+    for(let g of gnb){g.style.transition = 'color 0.3s';}
+    for(let la of lang){la.style.color = '#fff';}
+    for(let la of lang){la.style.border = '1px solid #fff';}
+    for(let la of lang){la.style.transition = 'all 0.3s';}
+})
