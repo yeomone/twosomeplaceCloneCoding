d53ca9f173bc96b74215caee7f17b18121a36bcd
won <won00yeom@naver.com>
2024-01-23 17:32:46 +0900

    vision 완성

--- script/common.js
+++ script/common.js
@@ -1,18 +1,17 @@
+// header, lnbBg(lnb들 담고 있는 거), subAllBg(흰배경), gnb(글씨 색 바뀌어야됨)
 const header = document.querySelector('header')
 const lnbBg = document.querySelectorAll('.lnbBg')
 const subAllBg = document.querySelector('.subAllBg')
 const gnb = document.querySelectorAll('.gnb > li > a')
+// 로고, 언어, 전체메뉴
 const lang = document.querySelectorAll('.lang')
 const logoDefault = document.querySelector('.default')
 const logoHvr = document.querySelector('.hvr')
 const allMenuDefault = document.querySelector('.allMenuDefault')
 const allMenuHvr = document.querySelector('.allMenuHvr')
-// allMenuDefault
-// allMenuHvr
-// default
-// hvr
 
-for(let l of lnbBg){l.style.display = 'none';}
+// for(let l of lnbBg){l.style.display = 'none';}
+for(let l of lnbBg){l.style.height = '0';}
 subAllBg.style.height = '0'
 subAllBg.style.transition = 'height 0.3s'
 
@@ -21,14 +20,6 @@ logoHvr.style.display = 'none'
 
 allMenuDefault.style.display = 'block'
 allMenuHvr.style.display = 'none'
-
-/* for(let h of header){
-    for(let l of subAllBg){
-        header.addEventListener('mouseover',()=>{
-            l.style.height = '500px'
-        })
-    }
-} */
 
 header.addEventListener('mouseover',()=>{
     logoDefault.style.display = 'none'
@@ -40,11 +31,13 @@ header.addEventListener('mouseover',()=>{
     subAllBg.style.height = '390px'
     subAllBg.style.transition = 'all 0.3s'
 
-    for(let l of lnbBg){l.style.display = 'block';}
+    // for(let l of lnbBg){l.style.display = 'block';}
+    for(let l of lnbBg){l.style.height = '500px';}
+    for(let l of lnbBg){l.style.transition = 'height';}
     for(let g of gnb){g.style.color = '#000';}
+    for(let g of gnb){g.style.transition = 'color 0.3s';}
     for(let la of lang){la.style.color = '#000';}
     for(let la of lang){la.style.border = '1px solid #000';}
-    for(let g of gnb){g.style.transition = 'color 0.3s';}
     for(let la of lang){la.style.transition = 'all 0.3s';}
 })
 header.addEventListener('mouseout',()=>{
@@ -56,8 +49,10 @@ header.addEventListener('mouseout',()=>{
     subAllBg.style.opacity = '0'
     subAllBg.style.height = '100px'
     subAllBg.style.transition = 'all 0.3s'
-
-    for(let l of lnbBg){l.style.display = 'none';}
+    
+    // for(let l of lnbBg){l.style.display = 'none';}
+    for(let l of lnbBg){l.style.height = '0';}
+    for(let l of lnbBg){l.style.transition = 'height';}
     for(let g of gnb){g.style.color = '#fff';}
     for(let g of gnb){g.style.transition = 'color 0.3s';}
     for(let la of lang){la.style.color = '#fff';}
