537339f497c85694d4c942bdf63aa8e0f249d88d
won <won00yeom@naver.com>
2024-03-07 18:14:14 +0900

    전체메뉴 애니메이션 추가

--- script/common.js
+++ script/common.js
@@ -2,7 +2,7 @@
 const header = document.querySelector('header')
 const lnbBg = document.querySelectorAll('.lnbBg')
 const subAllBg = document.querySelector('.subAllBg')
-const gnb = document.querySelectorAll('.gnb > li > a')
+const gnb = header.querySelectorAll('.gnb > li > a')
 // 로고, 언어, 전체메뉴
 const lang = document.querySelectorAll('.lang')
 const logoDefault = document.querySelector('.default')
@@ -11,6 +11,34 @@ const allMenuDefault = document.querySelector('.allMenuDefault')
 const allMenuHvr = document.querySelector('.allMenuHvr')
 const allMenuContainer = document.querySelector('.allMenuContainer')
 const allMenuClose = document.querySelector('#allMenuClose')
+const topBtn = document.querySelector('.topBtn')
+const allMenuGnb = allMenuContainer.querySelectorAll('.gnb > li > a')
+const allMenuGnbBg = allMenuContainer.querySelector('.allMenuWrap > .leftMenu')
+const allMenuLnb = allMenuContainer.querySelectorAll('.allMenuWrap > .leftMenu > .gnb > li > .lnb ')
+console.log(allMenuGnbBg)
+
+// 전체메뉴 - gnb에 마우스오버 시 lng 나타나기
+const lnbHide = () => {for(let i of allMenuLnb){i.style.opacity = 0;}}
+const allMenuGnbColor = () => {for(let i of allMenuGnb){i.style.color = '#707070';}}
+const allMenuGnbDefault = () => {for(let i of allMenuGnb){i.style.color = '#fff';}}
+const allMenuGnbDefault2 = () => {for(let i of allMenuGnb){i.style.transform = 'translateX(0)';}}
+lnbHide();
+
+allMenuGnb.forEach((t,i)=>{
+    t.addEventListener('mouseover',()=>{
+        lnbHide();
+        allMenuGnbColor();
+        allMenuGnbDefault2();
+        allMenuGnb[i].style.color = '#fff'
+        allMenuGnb[i].style.transform = 'translateX(50px)'
+        allMenuLnb[i].style.opacity = 1
+        allMenuLnb[i].style.transition = 'opacity 0.17s linear'
+    })
+    allMenuGnbBg.addEventListener('mouseleave',()=>{
+        allMenuGnb[i].style.transform = 'translateX(0)'
+        allMenuGnbDefault();
+    })
+})
 
 // 전체메뉴 아이콘 클릭하면 나타나기
 allMenuHvr.addEventListener('click',()=>{
