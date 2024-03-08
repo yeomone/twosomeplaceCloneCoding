cee962fa4a2b7b437d03b63021fc0718c31bb3fc
won <won00yeom@naver.com>
2024-03-08 15:19:51 +0900

    footer 연결

--- script/common.js
+++ script/common.js
@@ -55,47 +55,21 @@ for(let l of lnbBg){l.style.height = '0';}
 subAllBg.style.height = '0'
 subAllBg.style.transition = 'height 0.3s'
 
-// logoDefault.style.display = 'block'
-// logoHvr.style.display = 'none'
-
-// allMenuDefault.style.display = 'block'
-// allMenuHvr.style.display = 'none'
-
 header.addEventListener('mouseover',()=>{
-    // logoDefault.style.display = 'none'
-    // logoHvr.style.display = 'block'
-    // allMenuDefault.style.display = 'none'
-    // allMenuHvr.style.display = 'block'
 
     subAllBg.style.opacity = '1'
     subAllBg.style.height = '390px'
     subAllBg.style.transition = 'all 0.3s'
 
-    // for(let l of lnbBg){l.style.display = 'block';}
     for(let l of lnbBg){l.style.height = '500px';}
     for(let l of lnbBg){l.style.transition = 'height';}
-    // for(let g of gnb){g.style.color = '#000';}
-    // for(let g of gnb){g.style.transition = 'color 0.3s';}
-    // for(let la of lang){la.style.color = '#000';}
-    // for(let la of lang){la.style.border = '1px solid #000';}
-    // for(let la of lang){la.style.transition = 'all 0.3s';}
 })
 header.addEventListener('mouseout',()=>{
-    // logoDefault.style.display = 'block'
-    // logoHvr.style.display = 'none'
-    // allMenuDefault.style.display = 'block'
-    // allMenuHvr.style.display = 'none'
 
     subAllBg.style.opacity = '0'
     subAllBg.style.height = '100px'
     subAllBg.style.transition = 'all 0.3s'
     
-    // for(let l of lnbBg){l.style.display = 'none';}
     for(let l of lnbBg){l.style.height = '0';}
     for(let l of lnbBg){l.style.transition = 'height';}
-    // for(let g of gnb){g.style.color = '#fff';}
-    // for(let g of gnb){g.style.transition = 'color 0.3s';}
-    // for(let la of lang){la.style.color = '#fff';}
-    // for(let la of lang){la.style.border = '1px solid #fff';}
-    // for(let la of lang){la.style.transition = 'all 0.3s';}
 })
