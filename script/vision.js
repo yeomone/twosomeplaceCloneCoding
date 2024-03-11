27edbf323aebe3a0d602d8b5f48a6869c9c8c5cd
won <won00yeom@naver.com>
2024-03-11 12:18:19 +0900

    JS 토글 수정

--- script/vision.js
+++ script/vision.js
@@ -3,5 +3,5 @@ const menueWrapA = document.querySelector('.menueWrap > a')
 const introducingMenu = document.querySelector('.introducingMenu')
 
 menueWrap.addEventListener('click',()=>{
-    introducingMenu.style.display = 'flex'
+    introducingMenu.classList.toggle('show');
 })
